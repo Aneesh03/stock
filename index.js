026e838bc83099da7c2539f6d29f6ecb80b676ca
Aneesh03 <53593613+Aneesh03@users.noreply.github.com>
2021-09-19 16:16:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -35,12 +35,20 @@ app.use("/api/user", authRoute);
 app.use("/api/home", postRoute);
 // write to database from csv
 // --> Add this
+// if (process.env.NODE_ENV === "production") {
+//   // Serve any static files
+//   app.use(express.static(path.join(__dirname, "client/build")));
+//   // Handle React routing, return all requests to React app
+//   app.get("*", function (req, res) {
+//     res.sendFile(path.join(__dirname, "client/build", "index.html"));
+//   });
+// }
 if (process.env.NODE_ENV === "production") {
   // Serve any static files
   app.use(express.static(path.join(__dirname, "client/build")));
   // Handle React routing, return all requests to React app
   app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
   });
 }
 #initial();
