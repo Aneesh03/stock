b13e6fe253220a66f06c7e6f0443e26304a2a3c6
Aneesh03 <53593613+Aneesh03@users.noreply.github.com>
2021-09-22 20:46:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -37,14 +37,6 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
-if (process.env.NODE_ENV === "production") {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, "client/build")));
-  // Handle React routing, return all requests to React app
-  app.get("*", function (req, res) {
-    res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
-  });
-}
 #initial();
 app.listen(process.env.PORT || 8000, () =>
   console.log("Server is Up and running")
